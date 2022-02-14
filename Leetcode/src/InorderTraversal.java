3a76ea06e54d1d4c45777842023b6988265afe0a
tanujsingh <tanujsingh@192.168.0.183>
2022-02-13 22:41:44 -0700

    initial

---
+++ Leetcode/src/InorderTraversal.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+public class InorderTraversal {
+	public class TreeNode{
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(){}
+		TreeNode(int val){
+			this.val = val;
+		}
+		TreeNode(int val, TreeNode left, TreeNode right){
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	List<Integer> list = new LinkedList<>();
+	public List<Integer> inorderTraversal(TreeNode root){
+		if(root == null) return list;
+		inorderTraversal(root.left);
+		list.add(root.val);
+		inorderTraversal(root.right);
+		
+		return list;
+	}
+	
+}
