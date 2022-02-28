229cb9f64dbdedf5059ecaa1726db62b62b05bf6
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-02-27 20:00:18 -0700

    Time: 0 ms (100.00%), Space: 41.1 MB (71.04%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -1,17 +1,26 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] ver1 = version1.split("\\.");
-        String[] ver2 = version2.split("\\.");
-        int len1 = ver1.length;
-        int len2 = ver2.length;
         
-        int parseVer1, parseVer2;
-        for(int i = 0; i < Math.max(len1, len2); ++i){
-            parseVer1 = i < len1 ? Integer.parseInt(ver1[i]) : 0;
-            parseVer2 = i < len2 ? Integer.parseInt(ver2[i]) : 0;
+        int len1 = version1.length();
+        int len2 = version2.length();
+        int i = 0, j = 0; 
+        while(i < len1 || j < len2){
+            int temp1 = 0;
+            int temp2 = 0;
             
-            if(parseVer1 != parseVer2)
-                return parseVer1 > parseVer2 ? 1 : -1;
+            while(i < len1 && version1.charAt(i) != '.'){
+                temp1 = temp1* 10 + version1.charAt(i++) - '0';
+            }
+            
+            while(j < len2 && version2.charAt(j) != '.'){
+                temp2 = temp2* 10 + version2.charAt(j++) - '0';
+            }
+            
+            if(temp1 != temp2){
+                return temp1 > temp2 ? 1 : -1;
+            } 
+            i++;
+            j++;    
         }
         
         return 0;
