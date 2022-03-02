f1b35dfb3259b24ae9d4b3cb0144b7e835aade9d
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-02 11:11:29 -0700

    Time: 0 ms (100.00%), Space: 39.3 MB (46.93%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -3,7 +3,7 @@ public class Solution {
     public int hammingWeight(int n) {
         int count = 0;
         for(int i = 0; i < 32; i++){
-            if((n & 1) != 0)
+            if((n & 1) == 1)
                 count++;
             n >>= 1;
         }
