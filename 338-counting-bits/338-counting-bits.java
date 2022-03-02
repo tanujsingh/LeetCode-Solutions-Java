2ae5fac08c6680d664dbb0ea812de690f532a786
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-02 08:25:54 -0700

    Time: 5 ms (28.74%), Space: 47.6 MB (44.74%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -3,17 +3,24 @@ class Solution {
         int[] res = new int[n + 1];
         
         for(int i = 0; i <= n; ++i) {
-            res[i] = solve(i);
+            res[i] = solve(i, res);
         }
         
         return res;
     }
     
-    private int solve(int n) {
+    private int solve(int n, int[] memo) {
         if (n == 0) return 0;
         if (n == 1) return 1;
         
-        if (n % 2 == 0) return solve(n/2);
-        else return solve(n/2) + 1;
+        if(memo[n] != 0) return memo[n];
+        
+        if (n % 2 == 0) {
+            memo[n] = solve(n/2, memo);
+            return solve(n/2, memo);
+        } else {
+            memo[n] = 1 + solve(n/2, memo);
+            return solve(n/2, memo) + 1;
+        }
     }
 }
