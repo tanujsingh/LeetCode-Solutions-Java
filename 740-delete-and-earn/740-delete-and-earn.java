21627f0b3e645764e5790db22e292a45e987b0de
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-06 16:46:06 -0700

    Time: 8 ms (32.50%), Space: 44.6 MB (40.34%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -9,13 +9,15 @@ class Solution {
             maxNum = Math.max(maxNum, num);
         }
         
-        int[] maxPoints = new int[maxNum + 1]; // 0 to maxNum;
-        maxPoints[1] = points.getOrDefault(1, 0);
-
-        for(int num = 2; num < maxPoints.length; num++) {
+        int twoBack = 0;
+        int oneBack = points.getOrDefault(1, 0);
+        int curr = maxNum == 1 ? oneBack : 0;
+        for(int num = 2; num < maxNum + 1; num++) {
             int gain = points.getOrDefault(num, 0);
-            maxPoints[num] = Math.max(maxPoints[num - 1], maxPoints[num - 2] + gain);
+            curr = Math.max(oneBack, twoBack + gain);
+            twoBack = oneBack;
+            oneBack = curr;
         }
-        return maxPoints[maxNum];
+        return curr;
     }
 }
