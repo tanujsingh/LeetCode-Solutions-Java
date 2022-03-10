266e74c3c5cb814a281c22adf76b3839e8707ec8
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-09 21:06:55 -0700

    Time: 24 ms, Memory: 59.3 MB - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,14 +1,15 @@
-/*
-Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-Notice that the solution set must not contain duplicate triplets.
-*/
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> output = new ArrayList<>();
-        if(nums.length < 3) return output;
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 2; ++i) {
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
+        if(nums.length < 3) {
+            return output;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             
             int left = i + 1;
             int right = nums.length - 1;
@@ -16,14 +17,12 @@ class Solution {
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 
-                if(sum == 0) {
+                if (sum == 0) {
                     output.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
-                    
-                    while(left < right && nums[left] == nums[left - 1]) {
+                    while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
-                    
-                    while(left < right && nums[right] == nums[right + 1]) {
+                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
                 } else if (sum < 0) {
@@ -33,7 +32,6 @@ class Solution {
                 }
             }
         }
-        
         return output;
     }
 }
