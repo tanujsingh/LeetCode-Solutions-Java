a5d7becdcdcc1a5ba36faefd24e56d90371ebd06
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-11 00:20:10 -0700

    Time: 14 ms (79.23%), Space: 43.4 MB (49.49%) - LeetHub

--- 819-most-common-word/819-most-common-word.java
+++ 819-most-common-word/819-most-common-word.java
@@ -1,24 +1,29 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
         Map<String, Integer> map = new HashMap<>();
+        
+        Set<String> set = new HashSet<>();
+        for(String s : banned) {
+            set.add(s); 
+        }
+        
         String[] strs = paragraph.toLowerCase().split("\\W+");
         int max = 0;
         String res = "";
+
         for(String str : strs) {
-            map.put(str, map.getOrDefault(str, 0) + 1);
+            if(!set.contains(str)) {
+                map.put(str, map.getOrDefault(str, 0) + 1);
+            }
         }
-        for(String s : banned) {
-            if(map.containsKey(s)) {
-                map.remove(s);
-            } 
-        }
+        
+        
         for(String key : map.keySet()) {
             max = Math.max(max, map.get(key));
             if(max == map.get(key)) {
                 res = key;
             }
         }
-        
         
         return res;
     }
