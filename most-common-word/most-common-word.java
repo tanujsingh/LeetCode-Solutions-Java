0777e46114c0f855b3f96a75197d59a3bf3d250a
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-11 00:35:12 -0700

    Time: 18 ms, Memory: 43.4 MB - LeetHub

---
+++ most-common-word/most-common-word.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Set<String> set = new HashSet<>();
+        for(String str : banned) {
+            set.add(str);
+        }
+        
+        Map<String, Integer> map = new HashMap<>();
+        String[] strs = paragraph.toLowerCase().split("\\W+");
+        int max = 0;
+        String res = "";
+        
+        for(String str : strs) {
+            if(!set.contains(str)) {
+                map.put(str, map.getOrDefault(str, 0) + 1);
+            }
+        }
+        
+        for(String key : map.keySet()) {
+            if(max < map.get(key)) {
+                max = map.get(key);
+                res = key;
+            }
+        }
+        return res;
+    }
+}
