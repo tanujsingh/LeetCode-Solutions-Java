9f1e3017bf5e5e13639310dcc974a207c05ba233
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-11 00:16:03 -0700

    Time: 21 ms (59.74%), Space: 43.7 MB (45.05%) - LeetHub

--- 819-most-common-word/819-most-common-word.java
+++ 819-most-common-word/819-most-common-word.java
@@ -6,14 +6,12 @@ class Solution {
         String res = "";
         for(String str : strs) {
             map.put(str, map.getOrDefault(str, 0) + 1);
-            max = Math.max(max, map.get(str));
         }
         for(String s : banned) {
             if(map.containsKey(s)) {
                 map.remove(s);
             } 
         }
-        max = 0;
         for(String key : map.keySet()) {
             max = Math.max(max, map.get(key));
             if(max == map.get(key)) {
