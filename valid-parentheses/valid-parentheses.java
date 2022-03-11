169916bdd9cf77f8bb6476dbd046b05b8ca7062c
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-10 20:57:05 -0700

    Time: 1 ms, Memory: 39.9 MB - LeetHub

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        Map<Character, Character> map = new HashMap<>();
-        map.put('(',')');
-        map.put('{','}');
-        map.put('[',']');
         Stack<Character> stack = new Stack<>();
+        
         for(int i = 0; i < s.length(); ++i) {
             char ch = s.charAt(i);
             if(ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
-            } else if (ch == ')' || ch == '}' || ch == ']'){
-                if (stack.isEmpty() || map.get(stack.pop()) != ch) {
-                    return false;
-                }
-            } 
+            } else if (ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
         }
         
         if(!stack.isEmpty()) {
