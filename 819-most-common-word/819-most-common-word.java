984989f753f14ba1763b2afe36978b70ad2768cd
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-11 00:22:44 -0700

    Time: 17 ms (70.84%), Space: 43.9 MB (41.13%) - LeetHub

--- 819-most-common-word/819-most-common-word.java
+++ 819-most-common-word/819-most-common-word.java
@@ -1,12 +1,11 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        Map<String, Integer> map = new HashMap<>();
-        
         Set<String> set = new HashSet<>();
         for(String s : banned) {
             set.add(s); 
         }
         
+        Map<String, Integer> map = new HashMap<>();
         String[] strs = paragraph.toLowerCase().split("\\W+");
         int max = 0;
         String res = "";
@@ -19,8 +18,8 @@ class Solution {
         
         
         for(String key : map.keySet()) {
-            max = Math.max(max, map.get(key));
-            if(max == map.get(key)) {
+            if(map.get(key) > max) {
+                max = map.get(key);
                 res = key;
             }
         }
