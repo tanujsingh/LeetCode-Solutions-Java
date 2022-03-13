4b9fb275715dd8e1d3b6e97a6500057fcd151542
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-12 19:07:04 -0700

    Time: 11 ms (46.76%), Space: 49.5 MB (21.34%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -1,34 +1,27 @@
 
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-        PriorityQueue<String> letterLogs = new PriorityQueue<>(
-            Comparator
-            .<String,String>comparing(o -> o.split(" ",2)[1])
-            .thenComparing(o -> o.split(" ",2)[0]));
-        
-        List<String> digitLogs = new ArrayList<String>();
-        
-        for(String log : logs) {
-            if(Character.isDigit(log.split(" ", 2)[1].charAt(0))) {
-                digitLogs.add(log);
-            } else {
-                letterLogs.add(log);
-            }
-        }
-        
-        String[] result = new String[logs.length];
-        int i = 0;
-        while(!letterLogs.isEmpty()) {
-            result[i] = letterLogs.poll();
-            i++;
-        }
-        
-        for(String log : digitLogs) {
-            result[i] = log;
-            i++;
-        }
-        
-        return result;
+        Arrays.sort(logs, (s1, s2) -> {
+           String[] split1 = s1.split(" ", 2);
+           String[] split2 = s2.split(" ", 2);
+           
+           boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+           boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
             
+           if(!isDigit1 && !isDigit2) {
+               int comp = split1[1].compareTo(split2[1]);
+               if(comp == 0) {
+                   return split1[0].compareTo(split2[0]);
+               }
+               return comp;
+           } else if (isDigit1 && isDigit2) {
+               return 0;
+           } else if (isDigit1 && !isDigit2) {
+               return 1; //swap
+           } else {
+               return -1; //same order
+           }
+        });
+        return logs;    
     }
 }
