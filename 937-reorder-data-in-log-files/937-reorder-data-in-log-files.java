acd57ca61ad37ed7da11633717cb8b34ee6345e0
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-12 18:55:06 -0700

    Time: 11 ms (46.76%), Space: 49.6 MB (21.34%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -0,0 +1,34 @@
+
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        PriorityQueue<String> letterLogs = new PriorityQueue<>(
+            Comparator
+            .<String,String>comparing(o -> o.split(" ",2)[1])
+            .thenComparing(o -> o.split(" ",2)[0]));
+        
+        List<String> digitLogs = new ArrayList<String>();
+        
+        for(String log : logs) {
+            if(Character.isDigit(log.split(" ", 2)[1].charAt(0))) {
+                digitLogs.add(log);
+            } else {
+                letterLogs.add(log);
+            }
+        }
+        
+        String[] result = new String[logs.length];
+        int i = 0;
+        while(!letterLogs.isEmpty()) {
+            result[i] = letterLogs.poll();
+            i++;
+        }
+        
+        for(String log : digitLogs) {
+            result[i] = log;
+            i++;
+        }
+        
+        return result;
+            
+    }
+}
