e0dfcb07e2cc4c72647572f6f0c9479cb6e575f1
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-03-16 02:40:47 -0700

    Time: 51 ms (22.58%), Space: 54.4 MB (25.21%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,27 +1,23 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Set<Integer> set = new HashSet<>();
         Stack<Integer> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(s);
         for(int i = 0; i < s.length(); ++i) {
             if (s.charAt(i) == '(') {
                 stack.push(i);
-            } else if (s.charAt(i) == ')') {
-                if(stack.isEmpty()) {
-                    set.add(i);
-                } else {
+            }
+            if (s.charAt(i) == ')') {
+                if(!stack.isEmpty()) {
                     stack.pop();
+                } else {
+                    sb.setCharAt(i,'*');
                 }
             }
+            
         }
         while (!stack.isEmpty()) {
-            set.add(stack.pop());
+            sb.setCharAt(stack.pop(),'*');
         }
-        for (int i = 0; i < s.length(); ++i) {
-            if(!set.contains(i)) {
-                sb.append(s.charAt(i));
-            }
-        }
-        return sb.toString();
+        return sb.toString().replaceAll("\\*","");
     }
 }
