4ea9925f46356ff398f522ad2703bb69a8bd21ab
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-04-06 13:27:12 -0700

    Time: 2 ms (54.27%), Space: 51 MB (31.99%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,18 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet();
-        int sumNum = 0;
-        int sumArr = 0;
-        
+        int xor = 0;
         for(int num : nums) {
-            set.add(num);
-            sumArr += num;
-        }
-        
-        for(int num : set) {
-            sumNum += num;
-        }
-        
-        return 2*sumNum - sumArr;
+           xor ^= num;
+       }
+       return xor; 
     }
 }
