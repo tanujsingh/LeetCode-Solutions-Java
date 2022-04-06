2f358a02a23cccd5ce4e79e6f19248edf5b7eb09
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-04-06 13:59:10 -0700

    Time: 24 ms (86.13%), Space: 58.7 MB (66.85%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,26 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> set = new HashSet<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++) {
-            map.put(-nums[i], i);
+        List<List<Integer>> triplets = new ArrayList<>();
+        Arrays.sort(nums);
+        if(nums.length < 3) {
+            return triplets;
         }
-        
         for(int i = 0; i < nums.length; ++i) {
-            for(int j = i + 1; j < nums.length; ++j) {
-                int key = nums[i] + nums[j];
-                if(map.containsKey(key)) {
-                    int k = map.get(key);
-                    
-                    if(k != i && k != j) {
-                        List<Integer> tuple = Arrays.asList(nums[i], nums[j], nums[k]); 
-                        Collections.sort(tuple);
-                        set.add(tuple);
-                    }
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    triplets.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                    while(left < right && nums[left] == nums[left - 1]) left++;
+                    while(left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
-        
-        return new ArrayList(set);
+        return triplets;
     }
 }
