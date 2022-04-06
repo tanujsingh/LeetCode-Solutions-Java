25ca5a8e9e5cedae9793d7fddd56289a4d676695
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-04-06 12:57:20 -0700

    Time: 3 ms (79.99%), Space: 180.8 MB (49.13%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,27 +10,29 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        ListNode start = head;
-        ListNode end = head;
+        ListNode frontNode = head;
+        ListNode endNode = head;
         ListNode current = head;
-        int length = 0;
-        
-        while(current != null) {
-            length++;
-            if(end != null){
-                end = end.next;
+        int count = 0;
+        while (current != null) {
+            count++;
+            
+            if(endNode != null) {
+                endNode = endNode.next;
             }
-            if(length == k) {
-                start = current;
-                end = head;
+            
+            if(count == k) {
+                frontNode = current;
+                endNode = head;
             }
+            
             current = current.next;
         }
         
-        int temp = start.val;
-        start.val = end.val;
-        end.val = temp;
-
+        int temp = frontNode.val;
+        frontNode.val = endNode.val;
+        endNode.val = temp;
+        
         return head;
     }
 }
