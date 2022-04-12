86d823df55990c599ab7f667359a35cf31054a9b
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-04-12 01:37:55 -0700

    Time: 475 ms (54.27%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
-SELECT c.name AS "Customers" 
-FROM Customers AS c 
+SELECT name AS "Customers"
+FROM Customers AS c
 LEFT JOIN Orders AS o
 ON c.id = o.customerId
 WHERE o.customerId IS NULL;
+
