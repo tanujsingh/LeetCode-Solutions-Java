9f278cab88e340a889edfdfb37578394d5c0fc7a
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-04-12 04:40:02 -0700

    Time: 0 ms (100.00%), Space: 44 MB (15.22%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -5,13 +5,12 @@ class Solution {
         
         while (left <= right) {
             int mid = left + (right - left)/2;
-            
-            if(nums[mid] == target) {
+            if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
+            } else if (nums[mid] > target) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         return left;
