7f12b4eb5e196b11c7726ca4b364fa6aec2b9910
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-04-12 01:26:45 -0700

    Time: 1 ms (86.82%), Space: 43.4 MB (67.39%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,23 +1,13 @@
 class Solution {
-    Map<Integer, Integer> memo = new HashMap<>();
-   
     public int minCostClimbingStairs(int[] cost) {
-        return minCost(cost.length, cost);
-    }
-    
-    private int minCost(int i, int[] cost) {
-        if (i < 2) {
-            return 0;
+        int oneStepDown = 0;
+        int twoStepDown = 0;
+        int minCost = 0;
+        for (int i = 2; i < cost.length + 1; ++i) {
+            minCost = Math.min(oneStepDown + cost[i - 1], twoStepDown + cost[i - 2]);
+            twoStepDown = oneStepDown;
+            oneStepDown = minCost;
         }
-        
-        if (memo.containsKey(i)) {
-            return memo.get(i);
-        }
-        
-        int oneStepDown = cost[i - 1] + minCost(i - 1, cost);
-        int twoStepDown = cost[i - 2] + minCost(i - 2, cost);
-        memo.put(i, Math.min(oneStepDown, twoStepDown));
-        
-        return memo.get(i);
+        return minCost;
     }
 }
