dd49a36891c240627019a8abbed58522e7b0c20d
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-04-12 02:56:16 -0700

    Time: 19 ms (92.75%), Space: 46.3 MB (91.93%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,17 +1,19 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> triplets = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         for(int i = 0; i < nums.length; ++i) {
-            if(i > 0 && nums[i] == nums[i - 1]) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             int left = i + 1;
             int right = nums.length - 1;
-            while(left < right) {
+            
+            while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0) {
-                    triplets.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                    result.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                    
                     while(left < right && nums[left] == nums[left - 1]) left++;
                     while(left < right && nums[right] == nums[right + 1]) right--;
                 } else if (sum < 0) {
@@ -21,6 +23,6 @@ class Solution {
                 }
             }
         }
-        return triplets;
+        return result;
     }
 }
