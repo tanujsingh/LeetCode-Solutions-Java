597d154bd3b10f7c50737741e90de860e9544b80
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-04-12 04:37:33 -0700

    Time: 19 ms (43.42%), Space: 40.2 MB (79.81%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -6,10 +6,10 @@ public class Solution extends VersionControl {
         int left = 1;
         int right = n;
         
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left)/2;
             if(isBadVersion(mid)) {
-                right = mid;
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
