b8bd1bb843959de576be23e78e175d4176150e7f
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-04-23 12:49:23 -0700

    Time: 12 ms (16.91%), Space: 43.3 MB (58.54%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -1,29 +1,16 @@
 public class Codec {
-    String chars = "0123456789abcdefghijklmnopqrstuvwxyz";
-    Map<String, String> map = new HashMap<>();
-    int count = 0;
+    Map<Integer, String> map = new HashMap<>();
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        String key = getKey();
+        int key = longUrl.hashCode();
         map.put(key, longUrl);
-        count++;
         return "http://tinyurl.com/" + key;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        String decodedKey = shortUrl.replace("http://tinyurl.com/","");
+        int decodedKey = Integer.parseInt(shortUrl.replace("http://tinyurl.com/",""));
         return map.get(decodedKey);
-    }
-    
-    private String getKey() {
-        StringBuilder sb = new StringBuilder();
-        int i = count;
-        while (i > 0) {
-            sb.append(chars.charAt(i%36));
-            i /= 36;
-        }
-        return sb.toString();
     }
 }
 
