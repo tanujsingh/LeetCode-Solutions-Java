63dddcb4b8973da687c744dcda02e569cc9ede85
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-05-22 02:16:53 -0700

    Time: 6 ms (60.83%), Space: 42.3 MB (48.19%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,9 +1,9 @@
 class Solution {
     int count = 0;
     public int countSubstrings(String s) {
-        for (int i = 0; i < s.length(); ++i) {
-            extendPalindrome(s, i, i);
-            extendPalindrome(s, i, i + 1);
+        for (int i = 0; i < s.length(); ++i) { // i is the mid point
+            extendPalindrome(s, i, i); // odd length
+            extendPalindrome(s, i, i + 1); //even length
         }
         return count;
     }
