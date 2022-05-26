d23d79ba0c9cc73d5c8ba59c128798563f2547e0
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-05-26 01:05:24 -0700

    Time: 959 ms (5.01%), Space: 185.2 MB (7.32%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,18 +1,21 @@
 class Solution {
+    private int[][][] memo;
     public int findMaxForm(String[] strs, int m, int n) {
-		return helper(0, m, n, strs, new Integer[strs.length][m + 1][n + 1]);
+		memo = new int[strs.length][m + 1][n + 1];
+        return helper(0, m, n, strs);
 	}
 
-	private int helper(int index, int m, int n, String[] strs, Integer[][][] memo) {
+	private int helper(int index, int m, int n, String[] strs) {
+        
 		if (index == strs.length) return 0;
-		if (memo[index][m][n] != null) return memo[index][m][n];
+		if (memo[index][m][n] > 0) return memo[index][m][n];
 		
 		int zero = countZeros(strs[index]);
 		int one = strs[index].length() - zero;
 		
-		int res = helper(index + 1, m, n, strs, memo);
+		int res = helper(index + 1, m, n, strs);
 		if (m >= zero && n >= one)
-			res = Math.max(res, 1 + helper(index + 1, m - zero, n - one, strs, memo));
+			res = Math.max(res, 1 + helper(index + 1, m - zero, n - one, strs));
 		
 		return memo[index][m][n] = res;
 	}
