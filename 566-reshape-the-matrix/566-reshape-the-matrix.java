c8c9fbfdfe50bab3e1b5fc71ea08459b379ffd87
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-05-27 01:19:24 -0700

    Time: 1 ms (79.61%), Space: 51.2 MB (12.60%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -3,13 +3,21 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
         int[][] res = new int[r][c];
+        int rows = 0, cols = 0;
         
         if (m * n != r * c) {
             return mat;
         }
         
-        for (int i = 0; i < r * c; ++i) {
-            res[i/c][i%c] = mat[i/n][i%n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                res[rows][cols++] = mat[i][j];
+                
+                if (cols == c) {
+                    rows++;
+                    cols = 0;
+                }
+            }
         }
         return res;
     }
