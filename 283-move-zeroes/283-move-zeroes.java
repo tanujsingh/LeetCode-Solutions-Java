5e93df224177eb4c5585f021874bfc658d688a21
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-05-30 14:33:30 -0700

    Time: 2 ms (69.57%), Space: 54.7 MB (46.94%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+        
+        for (int num : nums) {
+            if (num != 0) {
+                nums[pos++] = num;
+            }
+        }
+        
+        for (int i = pos; i < nums.length; ++i) {
+            nums[i] = 0;
+        }
+    }
+}
