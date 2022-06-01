24cf47032eeecfedf4b0b98349fe179808472f07
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-06-01 11:20:27 -0700

    Time: 0 ms (100.00%), Space: 42.3 MB (90.13%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.java
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.java
@@ -23,7 +23,7 @@ class Solution {
         if(root2 == null) {
             return root1;
         }
-         
+        
         root1.val += root2.val;
         
         root1.left = mergeTrees(root1.left, root2.left);
