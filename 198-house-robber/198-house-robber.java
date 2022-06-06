8b8a205c4cc3a733a3e467c8c2487eecd84c944f
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-06-06 16:48:39 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (23.60%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,18 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] maxProfit = new int[n + 1];
         
         if(n == 0) {
             return 0;
         }
-        maxProfit[n] = 0;
-        maxProfit[n - 1] = nums[n - 1];
+        int robNextPlusOne = 0;
+        int robNext = nums[n - 1];
         
         for(int i = n - 2; i >= 0; --i) {
-            maxProfit[i] = Math.max(maxProfit[i + 1], maxProfit[i + 2] + nums[i]);
+            int robCurr = Math.max(robNext, robNextPlusOne + nums[i]);
+            robNextPlusOne = robNext;
+            robNext = robCurr;
         }
         
-        return maxProfit[0];
+        return robNext;
     }
 }
