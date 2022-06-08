b76b7ae792b66aa7661c055ca077468c64da71d6
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-06-08 00:17:30 -0700

    Time: 0 ms (100.00%), Space: 46.7 MB (67.28%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,24 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 0) {
-            return new int[]{-1,-1};
-        }
+        int first = findTarget(nums, target, true);
+        int last = findTarget(nums, target, false);
+        return new int[]{first, last};
+    }
+    
+    private int findTarget(int[] nums, int target, boolean isFirst){
+        int left = 0;
+        int right = nums.length - 1;
         
-        int[] res = new int[2];
-        int n = nums.length;
-        boolean begin = false;
-        boolean end = false;
-        for(int i = 0; i < n; ++i) {
-            if(nums[i] == target && begin == false) {
-                res[0] = i;
-                begin = true;
-            }
+        while(left <= right) {
+            int mid = left + (right - left)/2;
             
-            if(nums[n - i - 1] == target && end == false) {
-                res[1] = n - i - 1;
-                end = true;
+            if(nums[mid] == target) {
+                if(isFirst) {
+                    if(mid == 0 || nums[mid - 1] != target) {
+                        return mid;
+                    } else {
+                        right = mid - 1;
+                    }
+                } else {
+                    if(mid == nums.length - 1 || nums[mid + 1] != target) {
+                        return mid;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return begin == false && end == false ? new int[]{-1,-1} : res;
+        return -1;
     }
 }
