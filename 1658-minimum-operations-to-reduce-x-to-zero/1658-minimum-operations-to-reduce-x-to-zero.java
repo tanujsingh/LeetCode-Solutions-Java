eae4983294773d717b7481091dd4f10e8144b0ef
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-06-11 12:20:05 -0700

    Time: 71 ms (44.91%), Space: 58.2 MB (81.25%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,26 +1,18 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int maxLen = -1;
-        int left = 0;
-        int right = 0;
-        int sumArr = 0;
-        int winSum = 0;
-        for(int num : nums) {
-            sumArr += num;
+        int sumNums = 0, curs = 0;
+        for (int n: nums) sumNums += n;
+        Map<Integer, Integer> memo = new HashMap<>();
+        memo.put(0,-1);
+        if (sumNums == x) return nums.length;
+        int target = sumNums - x, maxWindowSize = 0;
+        for (int i = 0; i<nums.length; i++){
+            curs += nums[i];
+            memo.put(curs,i);
+            if (memo.containsKey(curs - target))
+                maxWindowSize = Math.max(maxWindowSize, i-memo.get(curs - target));
         }
         
-        while(left <= right && right < nums.length){
-            winSum += nums[right++];
-            
-            while(left <= right && winSum > sumArr - x) {
-                winSum -= nums[left++];
-            }
-            
-            if(winSum == sumArr - x) {
-                maxLen = Math.max(maxLen, right - left);
-            }
-        }
-        
-        return maxLen == -1 ? -1 : nums.length - maxLen;
+        return maxWindowSize>0?(nums.length-maxWindowSize):-1;
     }
 }
