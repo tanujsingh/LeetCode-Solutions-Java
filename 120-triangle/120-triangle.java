6f2a877c053e3b10b9ec53052a64dab1f1da71a5
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-06-13 08:31:57 -0700

    Time: 8 ms (24.53%), Space: 44.1 MB (47.12%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,29 +1,15 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int m = triangle.size();
-        int n = triangle.get(m - 1).size();
-        int[][] dp = new int[m][n];
-        
-        dp[0][0] = triangle.get(0).get(0);
-        
-        for(int i = 1; i < m; ++i) {
-            List<Integer> curr = triangle.get(i);
-            for(int j = 0; j < curr.size(); ++j) {
-                if(j == 0) {
-                    dp[i][j] = dp[i - 1][j] + curr.get(j);     
-                } else if (j == curr.size() - 1){
-                    dp[i][j] = dp[i - 1][j - 1] + curr.get(j);
-                } else {
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + curr.get(j);
-                } 
-            }
-        }
-       
-        int min = Integer.MAX_VALUE;
-        for(int i = 0; i < n; ++i) {
-            min = Math.min(dp[m - 1][i], min);
-        }
-        
-        return min;
+     public int minimumTotal(List<List<Integer>> triangle) {
+		if(triangle.size() == 0)
+			return 0;
+		
+		for (int i=triangle.size() - 2; i>=0; i--) {
+            List<Integer> nextRow = triangle.get(i+1);
+			for (int j=0; j<=i; j++) {
+				int sum = Math.min(nextRow.get(j), nextRow.get(j+1)) + triangle.get(i).get(j);
+				triangle.get(i).set(j, sum);
+			}
+		}
+		return triangle.get(0).get(0);
     }
 }
