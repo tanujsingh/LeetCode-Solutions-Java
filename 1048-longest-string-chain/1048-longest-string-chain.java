73f80fb4ade66efd4218c801ea3817998128d623
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-06-14 17:44:26 -0700

    Time: 61 ms (56.29%), Space: 72.7 MB (5.35%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -2,21 +2,26 @@ class Solution {
     public int longestStrChain(String[] words) {
         Arrays.sort(words, (a, b) -> a.length() - b.length());
         
-        Map<String, Integer> dp = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        
         for (String word : words) {
-            dp.put(word, 1);
+            map.put(word, 1);
         }
     
         int max = 0;
+        
         for (String word : words) {
             for (int i = 0; i < word.length(); ++i) { 
-                String prev = word.substring(0, i) + word.substring(i+1);
-                if (dp.containsKey(prev)) {
-                    dp.put(word, Math.max(dp.get(word), dp.get(prev) + 1));
+                String previous = word.substring(0, i) + word.substring(i+1);
+
+                if (map.containsKey(previous)) {
+                    map.put(word, Math.max(map.get(word), 1 + map.get(previous)));
                 }
             }
-            max = Math.max(max, dp.get(word));
+            
+            max = Math.max(max, map.get(word));
         }
+        
         return max;
     }
 }
