241301c887437464cb25350a38349d5b0c631c78
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-08-28 15:38:08 -0700

    Time: 3 ms (53.66%), Space: 58 MB (31.07%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,6 +1,30 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        
+        left[0] = 1;
+        for(int i = 1; i < n; ++i) {
+            left[i] = left[i - 1]*nums[i - 1];
+        }
+        
+        right[n - 1] = 1;
+        for(int i = n - 2; i >= 0; --i) {
+            right[i] = right[i + 1]*nums[i + 1];
+        }
+        
+        for(int i = 0; i < n; ++i) {
+            nums[i] = left[i]*right[i];
+        }
+        
+        return nums;
+    }
+    
+    /* ------------------------------ T: O(n), S: O(1) -----------------------------------*/
+    /*
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
         int[] res = new int[n];
         res[0] = 1;
         
@@ -16,6 +40,7 @@ class Solution {
         
         return res;
     }
+    */
     
     /* ------------------------------ Brute Force -----------------------------------*/
     /*
