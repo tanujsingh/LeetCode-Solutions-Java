1e11be604aec2b191f6e95f5ec8d6f7dde9a1cdd
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-08-28 15:39:46 -0700

    Time: 11 ms (6.23%), Space: 58 MB (31.07%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,5 +1,26 @@
 class Solution {
+   /* ------------------------------ T: O(n), S: O(1) -----------------------------------*/
     public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        
+        for(int i = 1; i < n; ++i) {
+            res[i] = res[i - 1]*nums[i - 1];
+        }
+        
+        int num = 1;
+        for(int i = n - 1; i >= 0; --i) {
+            res[i] = res[i]*num;
+            num = num*nums[i];
+        }
+        
+        return res;
+    }
+    
+    /* ------------------------------ T: O(n), S: O(n) -----------------------------------*/
+     /*
+     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] left = new int[n];
         int[] right = new int[n];
@@ -19,26 +40,6 @@ class Solution {
         }
         
         return nums;
-    }
-    
-    /* ------------------------------ T: O(n), S: O(1) -----------------------------------*/
-    /*
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        res[0] = 1;
-        
-        for(int i = 1; i < n; ++i) {
-            res[i] = res[i - 1]*nums[i - 1];
-        }
-        
-        int num = 1;
-        for(int i = n - 1; i >= 0; --i) {
-            res[i] = res[i]*num;
-            num = num*nums[i];
-        }
-        
-        return res;
     }
     */
     
