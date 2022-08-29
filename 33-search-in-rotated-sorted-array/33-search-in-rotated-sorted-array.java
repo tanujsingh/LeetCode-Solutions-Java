1c0e57612fabcb8ef08e6c131471c4bd9e559684
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-08-28 19:44:12 -0700

    Time: 1 ms (76.40%), Space: 42.3 MB (71.74%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -4,13 +4,24 @@ class Solution {
         int left = 0;
         int right = n - 1;
         
-        while (left < right) {
+        if(left == right) return target == nums[0] ? 0 : -1;
+    
+        while(left <= right) {
             int mid = left + (right - left)/2;
+            if(mid < n - 1 && nums[mid] > nums[mid + 1]) {
+                left = mid + 1;
+                break;
+            }
             
-            if(nums[mid] > nums[right]) {
+            if(mid > 0 && nums[mid] < nums[mid - 1]) {
+                left = mid;
+                break;
+            }
+            
+            if(nums[mid] > nums[0]) {
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid - 1;
             }
         }
         
@@ -20,7 +31,8 @@ class Solution {
         
         while(left <= right) {
             int mid = left + (right - left)/2;
-            int newMid = (mid + rotation)%n; 
+            int newMid = (mid + rotation)%n;
+            
             if(nums[newMid] == target) {
                 return newMid;
             } else if (nums[newMid] < target) {
