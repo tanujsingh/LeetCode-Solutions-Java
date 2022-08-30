a5b90db1dc66b1938b19a19a1bdbe6f63695ccf3
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-08-30 11:45:56 -0700

    Time: 23 ms (74.64%), Space: 45.2 MB (52.79%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,4 +1,23 @@
 class Solution {
+    
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        
+        dp[0] = 0;
+        
+        for(int i = 1; i <= amount; ++i) {
+            for(int coin : coins) {
+                if(i - coin < 0) continue;
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+    
+    /*---------------------------------Memoization T: O(S * c) S: O(S) -----------------------------------*/
+    /*
     private Integer[] memo;
     public int coinChange(int[] coins, int amount) {
         memo = new Integer[amount + 1];
@@ -22,4 +41,5 @@ class Solution {
         memo[amount] = minCount == Integer.MAX_VALUE ? -1 : minCount;
         return memo[amount];
     }
+    */
 }
