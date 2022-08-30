bca317baf24bafd30666ebf121f2840ee8406d47
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-08-30 11:34:59 -0700

    Time: 67 ms (17.88%), Space: 45.9 MB (31.45%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -11,15 +11,15 @@ class Solution {
         
         if(memo[amount] != null) return memo[amount];
         
-        int minCoint = Integer.MAX_VALUE;
+        int minCount = Integer.MAX_VALUE;
         
         for(int coin : coins) {
             int count = helper(coins, amount - coin);
             if(count == -1) continue;
-            minCoint = Math.min(minCoint, count + 1);
+            minCount = Math.min(minCount, count + 1);
         }
         
-        memo[amount] = minCoint == Integer.MAX_VALUE ? -1 : minCoint;
+        memo[amount] = minCount == Integer.MAX_VALUE ? -1 : minCount;
         return memo[amount];
     }
 }
