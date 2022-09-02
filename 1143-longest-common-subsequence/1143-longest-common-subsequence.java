6a8862383f200268efb00712e49c7224132dead3
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-09-02 11:40:50 -0700

    Time: 17 ms (74.81%), Space: 46.7 MB (21.63%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,29 +2,19 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
-        int[][] memo = new int[m + 1][n + 1];
+        int[][] dp = new int[m + 1][n + 1];
         
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
-                memo[i][j] = -1;
+        for(int i = m - 1; i >= 0; --i) {
+            for(int j = n - 1; j >= 0; --j) {
+                if(text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
             }
         }
-        
-        return helper(0, 0, memo, text1, text2);
-    }
-    
-    private int helper(int p1, int p2, int[][] memo, String text1, String text2) {
-        if(memo[p1][p2] != -1) return memo[p1][p2];
-        
-        int ans = 0;
-        if(text1.charAt(p1) == text2.charAt(p2)) {
-            ans = 1 + helper(p1 + 1, p2 + 1, memo, text1, text2);
-        } else {
-            ans = Math.max(helper(p1 + 1, p2, memo, text1, text2), helper(p1, p2 + 1, memo, text1, text2));
-        }
-        
-        memo[p1][p2] = ans;
-        return ans;
+
+        return dp[0][0];
     }
     
     /*----------------------T: O(M.N2) S: O(M.N)------------------------*/
@@ -55,6 +45,37 @@ class Solution {
         
         memo[p1][p2] = Math.max(opt1, opt2);
         return memo[p1][p2];
+    }
+    */
+    
+    /*----------------------T: O(M.N) S: O(M.N)------------------------*/
+    /*
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] memo = new int[m + 1][n + 1];
+        
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                memo[i][j] = -1;
+            }
+        }
+        
+        return helper(0, 0, memo, text1, text2);
+    }
+    
+    private int helper(int p1, int p2, int[][] memo, String text1, String text2) {
+        if(memo[p1][p2] != -1) return memo[p1][p2];
+        
+        int ans = 0;
+        if(text1.charAt(p1) == text2.charAt(p2)) {
+            ans = 1 + helper(p1 + 1, p2 + 1, memo, text1, text2);
+        } else {
+            ans = Math.max(helper(p1 + 1, p2, memo, text1, text2), helper(p1, p2 + 1, memo, text1, text2));
+        }
+        
+        memo[p1][p2] = ans;
+        return ans;
     }
     */
 }
