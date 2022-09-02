20391adc0ea3769fb2afdc17ec6f1581b5d5f3ed
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-09-02 13:14:00 -0700

    Time: 0 ms (100.00%), Space: 39.5 MB (96.86%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,31 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0) {
-            return 0;
-        }
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0], nums[1]);
         
-        if(nums.length == 1) {
-            return nums[0];
-        }
-        
-        if(nums.length == 2) {
-            return Math.max(nums[0], nums[1]);
-        }
-        
-        int maxFirst = robScheme(nums, 0, nums.length - 1);
-        int maxLast = robScheme(nums, 1, nums.length);
+        int maxFirst = helper(nums, 0, nums.length - 1);
+        int maxLast = helper(nums, 1, nums.length);
         
         return Math.max(maxFirst, maxLast);
     }
     
-    private int robScheme(int[] nums, int begin, int end) {
+     private int helper(int[] nums, int begin, int end) {
         int n = end - begin;
         int[] dp = new int[n];
         dp[0] = nums[begin];
         dp[1] = Math.max(dp[0], nums[begin + 1]);
         
         for(int i = begin + 2; i < end; ++i) {
-            dp[i - begin] = Math.max(dp[i - begin - 1],nums[i] + dp[i - begin - 2]);
+            dp[i - begin] = Math.max(dp[i - begin - 1],
+                                     nums[i] + dp[i - begin - 2]);
         }
                 
         return dp[n - 1];
