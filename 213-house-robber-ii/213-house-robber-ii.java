9e0e9aa1825201e6bcd33c1dcaa1530e2d2e29f0
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-09-02 15:11:42 -0700

    Time: 0 ms (100.00%), Space: 42.3 MB (5.22%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -2,23 +2,26 @@ class Solution {
     public int rob(int[] nums) {
       if (nums.length == 1) 
           return nums[0];
-       int include_1st = nums[0] + helper(nums, 2, nums.length - 2);
+      if (nums.length == 2) {
+          return Math.max(nums[0], nums[1]);
+      }
+       int include_1st = helper(nums, 0, nums.length - 2);
        int exclude_1st = helper(nums, 1, nums.length - 1);
         
        return Math.max(include_1st, exclude_1st);
     }
     
     private int helper(int[] nums, int start, int end) {
-        int prev_last2 = 0;
-        int prev_last1 = 0;
+        int n = end - start;
+        int[] dp = new int[n + 1];
+        dp[0] = nums[start];
+        dp[1] = Math.max(dp[0], nums[start + 1]);
         
-        for(int i = start; i <= end; ++i) {
-            int curr = Math.max(prev_last1, prev_last2 + nums[i]);
-            int temp = prev_last1;
-            prev_last1 = curr;
-            prev_last2 = temp;
+        for(int i = start + 2; i <= end; ++i) {
+            dp[i - start] = Math.max(dp[i - start - 1], 
+                                     nums[i] + dp[i - start - 2]);
         }
         
-        return prev_last1;
+        return dp[n];
     }
 }
