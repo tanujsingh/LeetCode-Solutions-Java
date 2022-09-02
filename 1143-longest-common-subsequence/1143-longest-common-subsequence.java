9edb92c79b85ecd5e21dca0a02fb51825619a77b
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-09-02 11:32:12 -0700

    Time: 31 ms (20.84%), Space: 46.1 MB (73.96%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -16,6 +16,36 @@ class Solution {
     private int helper(int p1, int p2, int[][] memo, String text1, String text2) {
         if(memo[p1][p2] != -1) return memo[p1][p2];
         
+        int ans = 0;
+        if(text1.charAt(p1) == text2.charAt(p2)) {
+            ans = 1 + helper(p1 + 1, p2 + 1, memo, text1, text2);
+        } else {
+            ans = Math.max(helper(p1 + 1, p2, memo, text1, text2), helper(p1, p2 + 1, memo, text1, text2));
+        }
+        
+        memo[p1][p2] = ans;
+        return ans;
+    }
+    
+    /*----------------------T: O(M.N2) S: O(M.N)------------------------*/
+    /*
+        public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] memo = new int[m + 1][n + 1];
+        
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                memo[i][j] = -1;
+            }
+        }
+        
+        return helper(0, 0, memo, text1, text2);
+    }
+    
+    private int helper(int p1, int p2, int[][] memo, String text1, String text2) {
+        if(memo[p1][p2] != -1) return memo[p1][p2];
+        
         int opt1 = helper(p1 + 1, p2, memo, text1, text2);
         int firstOccurance = text2.indexOf(text1.charAt(p1), p2);
         int opt2 = 0;
@@ -26,4 +56,5 @@ class Solution {
         memo[p1][p2] = Math.max(opt1, opt2);
         return memo[p1][p2];
     }
+    */
 }
