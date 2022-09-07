d9b3ce6ffbcb89b5c1b9ece61d061b2bbc1cd652
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-09-07 13:21:39 -0700

    Time: 2 ms (57.52%), Space: 41.7 MB (31.11%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -0,0 +1,29 @@
+class Solution {
+    private Map<Integer, Integer> memo = new HashMap<>(); 
+    public int combinationSum4(int[] nums, int target) {
+        // Arrays.sort(nums);
+        
+        return totalCombinations(nums, target);
+    }
+    
+    private int totalCombinations(int[] nums, int remain) {
+        if(remain == 0) {
+            return 1;
+        }
+        
+        if(memo.containsKey(remain)) {
+            return memo.get(remain);
+        }
+        
+        int ans = 0;
+        for(int num : nums) {
+            if(remain - num >= 0) {
+                ans += totalCombinations(nums, remain - num);
+            }
+        }
+        
+        memo.put(remain, ans);
+        
+        return ans;
+    }
+}
