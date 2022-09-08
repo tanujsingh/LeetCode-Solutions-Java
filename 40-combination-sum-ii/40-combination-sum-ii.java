b7e830a767264adcc46bde17971bc8c80eee6053
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-09-07 20:24:30 -0700

    Time: 5 ms (74.04%), Space: 44.4 MB (19.57%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -17,9 +17,10 @@ class Solution {
         }
         
         for(int i = start; i < candidates.length; ++i) {
-            if(i > start && candidates[i] == candidates[i - 1]) {
+            if((i > start && candidates[i] == candidates[i - 1]) || candidates[i] > remain) {
                 continue;
             }
+
             if(remain - candidates[i] >= 0) {
                 tempList.add(candidates[i]);
                 helper(candidates, remain - candidates[i], tempList, i + 1);
