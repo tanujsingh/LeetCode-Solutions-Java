0c9f69a52e074dca52c5b5f615eb683286173569
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-09-13 11:58:48 -0700

    Time: 4 ms (71.43%), Space: 49.1 MB (82.98%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,34 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double ans = 0.0;
         int m = nums1.length;
         int n = nums2.length;
         
-        int[] resArr = new int[m + n];
+        if( m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
         
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        while(i < m && j < n) {
-            if(nums1[i] < nums2[j]) {
-                resArr[k++] = nums1[i++];
+        int low = 0;
+        int high = m;
+        
+        while (low <= high) {
+            int partitionM = (low + high) / 2;
+            int partitionN = (m + n + 1)/2 - partitionM;
+            
+            int maxLeftM = partitionM == 0 ? Integer.MIN_VALUE : nums1[partitionM - 1];
+            int maxLeftN = partitionN == 0 ? Integer.MIN_VALUE : nums2[partitionN - 1];
+            int minRightM = partitionM == m ? Integer.MAX_VALUE : nums1[partitionM];
+            int minRightN = partitionN == n ? Integer.MAX_VALUE : nums2[partitionN];
+            
+            if(maxLeftM <= minRightN && maxLeftN <= minRightM) {
+                if((m + n)%2 == 0) {
+                    return (double)(Math.max(maxLeftM, maxLeftN) + Math.min(minRightM, minRightN)) / 2;
+                } else {
+                    return (double)Math.max(maxLeftM, maxLeftN);
+                }
+            } else if(maxLeftM > minRightN) {
+                high = partitionM - 1;
             } else {
-                resArr[k++] = nums2[j++];
+                low = partitionM + 1;
             }
         }
-        
-        while(j < n) {
-            resArr[k++] = nums2[j++];
-        }
-        
-        while(i < m) {
-            resArr[k++] = nums1[i++];
-        }
-        
-        if(k % 2 == 0) {
-            return (double)(resArr[k/2] + resArr[k/2 - 1])/2;
-        } 
-        
-        return (double)resArr[k/2];
+        return 0.0;
     }
 }
