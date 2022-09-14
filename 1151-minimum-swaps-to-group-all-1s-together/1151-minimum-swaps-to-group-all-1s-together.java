72d0ed3c6e498ad7e452b9a86256a62226b66d45
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-09-14 15:37:11 -0700

    Time: 2 ms (100.00%), Space: 52.3 MB (89.05%) - LeetHub

---
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minSwaps(int[] data) {
+        int slidingWindowRange = 0;
+        
+        for(int item : data) {
+            slidingWindowRange += item;
+        }
+        
+        if(slidingWindowRange == 1)
+            return 0;
+        
+        int left = 0;
+        int right = 0;
+        int maxOnesInRange = 0;
+        int currOnesInRange = 0;
+        
+        for(int i = 0; i < slidingWindowRange; ++i) {
+            currOnesInRange += data[i];
+            maxOnesInRange = Math.max(maxOnesInRange, currOnesInRange);
+            right++;
+        }
+        
+        while(right != data.length) {
+            currOnesInRange += data[right++];
+            currOnesInRange -= data[left++];
+            maxOnesInRange = Math.max(maxOnesInRange, currOnesInRange);
+        }
+        
+        return slidingWindowRange - maxOnesInRange;
+    }
+}
