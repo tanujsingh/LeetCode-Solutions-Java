eec8874947193fbeaf212a7fae7fe85c28aaa495
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-09-14 14:19:43 -0700

    Time: 9 ms (48.60%), Space: 45.2 MB (33.10%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.java
+++ 657-robot-return-to-origin/657-robot-return-to-origin.java
@@ -7,21 +7,11 @@ class Solution {
         int sumVertical = 0;
         
         for(char ch : moves.toCharArray()) {
-            if(ch == 'L' || ch == 'R'){
-                if(ch == 'R')
-                    sumHorizontal += 1;
-                else
-                    sumHorizontal -= 1;
+            if(ch == 'R') sumHorizontal += 1;
+            else if(ch == 'L') sumHorizontal -= 1;
+            else if(ch == 'U') sumVertical += 1;
+            else sumVertical -= 1;
             }
-            
-            if(ch == 'U' || ch == 'D'){
-                if(ch == 'U')
-                    sumVertical += 1;
-                else
-                    sumVertical -= 1;
-            }
-            
-        }
         
         return sumVertical == 0 && sumHorizontal == 0;
     }
