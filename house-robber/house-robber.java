c7dd00744584b0c7b5379068f3454850f5490713
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-09-29 01:54:03 -0700

    Time: 2 ms, Memory: 41.5 MB - LeetHub

---
+++ house-robber/house-robber.java
@@ -0,0 +1,22 @@
+class Solution {
+    Map<Integer, Integer> memo = new HashMap<>();
+    int[] nums;
+    
+    private int dp(int i) {
+        if(i == 0) {
+            return nums[0];
+        }
+        if(i == 1) {
+            return Math.max(nums[0], nums[1]);
+        }
+        if(!memo.containsKey(i)) {
+            memo.put(i, Math.max(dp(i - 1), dp(i - 2) + nums[i]));
+        }
+        return memo.get(i);
+    }
+    
+    public int rob(int[] nums) {
+        this.nums = nums;    
+        return dp(nums.length - 1);    
+    }
+}
