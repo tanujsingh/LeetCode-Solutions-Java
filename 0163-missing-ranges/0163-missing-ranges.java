a91dc69805beb61bdd4474fb182c6c873fa0463b
Tanuj Singh <tanujsingh.iiita@gmail.com>
2022-10-17 21:32:11 -0700

    Time: 8 ms (67.49%), Space: 42.4 MB (42.32%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.java
+++ 0163-missing-ranges/0163-missing-ranges.java
@@ -1,31 +1,36 @@
 class Solution {
   public List<String> findMissingRanges(int[] nums, int lower, int upper) {
-        List<String> result = new ArrayList<>();
-        if (nums == null || nums.length == 0){
-            result.add(formRange(lower,upper));
-            return result;
-        }
-
-        // 1st step
-        if (nums[0] > lower){
-            result.add(formRange(lower,nums[0]-1));
-        }
-
-        // 2nd step
-        for (int i = 0; i < nums.length-1; i++){
-            if (nums[i+1] != nums[i] && nums[i+1] > nums[i] +1) {
-                result.add(formRange(nums[i]+1, nums[i+1]-1));
-            }
-        }
-
-       // 3rd step
-        if (nums[nums.length-1] < upper){
-            result.add(formRange(nums[nums.length-1]+1, upper));
-        }
-        return result;
-    }
+      List<String> list = new ArrayList<>();
+      
+      if(nums.length == 0) {
+          list.add(range(lower, upper));
+          return list;
+      }
+      
+      if(lower < nums[0]) {
+          list.add(range(lower, nums[0] - 1));
+      }
+      
+      for(int i = 0; i < nums.length - 1; ++i) {
+          if(nums[i + 1] > nums[i] + 1) {
+              list.add(range(nums[i] + 1, nums[i + 1] - 1));
+          }
+      }
+      
+      if(nums[nums.length - 1] < upper) {
+          list.add(range(nums[nums.length - 1] + 1, upper));
+      }
+      
+      return list;
+  }
     
-    public String formRange(int low, int high){
-        return low == high ? String.valueOf(low) : (low + "->" + high);
-    }
+  private String range(int lower, int upper) {
+      String str = "";
+      if(lower == upper) {
+          str = Integer.toString(lower);
+      } else {
+          str = lower + "->" + upper;
+      }
+      return str;
+  }  
 }
