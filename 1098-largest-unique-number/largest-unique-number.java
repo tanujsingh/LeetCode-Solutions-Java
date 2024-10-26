9545b47176a1b5f7ebd8c8b298da299525e38ce1
Tanuj Singh <tsingh29@asu.edu>
2024-10-25 20:55:25 -0400

    Time: 1 ms (99.21%) | Memory: 42.9 MB (49.36%) - LeetSync

--- 1098-largest-unique-number/largest-unique-number.java
+++ 1098-largest-unique-number/largest-unique-number.java
@@ -1,18 +1,13 @@
 class Solution {
     public int largestUniqueNumber(int[] nums) {
-        int[] arr = new int[2001];
-        Arrays.fill(arr, -1);
+        int[] count = new int[1001];
         
         for (int num : nums) {
-            if (arr[num] == -1) {
-                arr[num] = 0;
-            } else {
-                arr[num]++;
-            }
+            count[num]++;
         }
         
-        for (int i= 2000; i >= 0; --i) {
-            if (arr[i] == 0) {
+        for (int i= 1000; i >= 0; --i) {
+            if (count[i] == 1) {
                 return i;
             }
         }
